5a651412b29dcf39d487fdb80eb762106f9236e3
Marcus Chaves <marcus.chaves@digitro.com>
2026-06-16 14:00:52 -0300

    Evolução

---
+++ test_package/resources/cmake/resumoConfig.cmake
@@ -0,0 +1,7 @@
+# Minimal CMake package config to allow find_package(resumo CONFIG)
+# This file was generated in build-sanit2/install by the build helper.
+
+include("${CMAKE_CURRENT_LIST_DIR}/resumoTargets.cmake")
+
+# optional: provide version
+set(resumo_VERSION "0.0")
